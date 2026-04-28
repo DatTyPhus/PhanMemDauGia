7fc24f4df7bf9c54f15ac649b2272c99a6cda79f
Nguyen Minh Tri <25023411@vnu.edu.vn>
2026-04-28 22:28:39 +0700

    Upload items

---
+++ src/main/java/com/auction/shared/model/Art.java
@@ -0,0 +1,14 @@
+public class Art extends Item {
+    private String artist; //tác giả
+
+    public Art(int itemId, int sellerId, String itemName, String description,
+               BigDecimal startPrice, String imageUrl, String artist) {
+        super(itemId, sellerId, itemName, description, "Art", startPrice, imageUrl);
+        this.artist = artist;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("Art Work: " + itemName + " | Artist: " + artist);
+    }
+}
