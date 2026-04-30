eec118a69bd07da5c85e8712d25b63b65c3b3943
Nguyễn Tiến Đạt <dat243171@gmail.com>
2026-04-30 23:48:46 +0700

    All files : Sắp xếp lại file src và database

--- src/main/java/com/auction/shared/model/Entity.java
+++ src/main/java/com/auction/shared/model/Entity.java
@@ -3,7 +3,7 @@ package com.auction.shared.model;
 
 public abstract class Entity {
 
-    protected static Integer id;
+    protected static Integer id=0;
     public synchronized Entity() {
         id= id +1;
     }
