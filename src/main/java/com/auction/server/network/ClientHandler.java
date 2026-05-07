b9f61a73bb4c862ba0b62716bafcd947d24a3bf0
Nguyễn Tiến Đạt <dat243171@gmail.com>
2026-05-07 17:29:36 +0700

    server/network : Cấu hình nhận,gửi từ server

--- src/main/java/com/auction/server/network/ClientHandler.java
+++ src/main/java/com/auction/server/network/ClientHandler.java
@@ -1,10 +1,14 @@
 package com.auction.server.network;
 
+import com.auction.shared.network.Message;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
-// Implements Runnable để biến class này thành một tác vụ có thể chạy song song
+//Class dùng để đọc file JSON trên client gửi xuống , vừa kiểm tra nhãn dán rồi ném đến controller.
+
 public class ClientHandler implements Runnable {
     private Socket socket;
 
@@ -16,18 +20,28 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         // Bước 4 (Phía Server): Thiết lập ống Đọc dữ liệu (InputStream)
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String jsonReceived;
 
-            String jsonGoiLen;
-            // Vòng lặp liên tục đọc dữ liệu trong ống.
-            // readLine() sẽ chờ cho đến khi Client ấn "gửi" một dòng text.
-            while ((jsonGoiLen = in.readLine()) != null) {
-                System.out.println("Hộp thư Server nhận được: " + jsonGoiLen);
+            while ((jsonReceived = in.readLine()) != null) {
+                // Dùng Gson bóc hộp ngay lập tức
+                Message msg = Message.fromJson(jsonReceived);
 
-                // (Sau này, chúng ta sẽ bóc tách chuỗi JSON ở đây để gọi Logic Đăng nhập / Đặt giá)
-            }
-
-        } catch (Exception e) {
+                // Điều phối công việc dựa trên nhãn dán "action"
+                switch (msg.getAction()) {
+                    case "LOGIN":
+                        System.out.println("Nhận lệnh LOGIN với dữ liệu: " + msg.getPayload());
+                        break;
+                    case "BID":
+                        // Xử lý đặt giá...
+                        break;
+                    default:
+                        System.out.println("Không hiểu lệnh này!");
+                    }
+                }
+        }
+        catch(IOException e){
             System.out.println("Client " + socket.getInetAddress() + " đã ngắt kết nối đột ngột.");
         }
     }
